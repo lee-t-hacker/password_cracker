eee9653419ca9831a1deb2c2470c4d9d32898f2a
Lee T. Hacker <159954554+lee-t-hacker@users.noreply.github.com>
2024-02-13 14:52:45 -0500

    Testing passwords
    Trying out the cracker with my personal password. Hopefully it doesn't take too long.

--- pass_crack.py
+++ pass_crack.py
@@ -9,14 +9,16 @@ def generate_random_string(length):
     return random_string
 
 def check_md5(input):
-  input = input.encode('utf-8')
-  return hashlib.md5(input).hexdigest()
+    input = input.encode('utf-8')
+    return hashlib.md5(input).hexdigest()
 
 while True:
-  password = input("Enter the hash you would like to crack: ")
-  guess = generate_random_string(len(password))
-  guesshash = check_md5(guess)
+    #  password = input("Enter the hash you would like to crack: ")
+    test_password = LeesEpicPasswordThatIsVerySecure 
+    password = check_md5(test_password)
+    guess = generate_random_string(len(password))
+    guesshash = check_md5(guess)
   
-  if password == guesshash:
-    print(f'Password cracked! Password: {guess}')
-    break
+    if password == guesshash:
+        print(f'Password cracked! Password: {guess}')
+        break
