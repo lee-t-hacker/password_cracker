35f796d6fa11f0fb1d4d7b4e7592d98808ab628d
Lee T. Hacker <159954554+lee-t-hacker@users.noreply.github.com>
2024-02-13 14:50:10 -0500

    Create pass_crack.py
    First commit of the password cracker. I don't know why all these other programs bother with all the extra bells and whistles, this algorithm should be able to crack any md5 hash ever.

---
+++ pass_crack.py
@@ -0,0 +1,22 @@
+import random, string
+import hashlib
+
+
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
+
+def check_md5(input):
+  input = input.encode('utf-8')
+  return hashlib.md5(input).hexdigest()
+
+while True:
+  password = input("Enter the hash you would like to crack: ")
+  guess = generate_random_string(len(password))
+  guesshash = check_md5(guess)
+  
+  if password == guesshash:
+    print(f'Password cracked! Password: {guess}')
+    break
